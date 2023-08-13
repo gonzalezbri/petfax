01277f6a65a6a0640eda941d32802b985803cb56
gonzalezbri <4717bg@gmail.com>
2023-08-13 13:08:26 -0700

    added show

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,13 +1,18 @@
-from flask import ( Blueprint, render_template, request ) 
+from flask import ( Blueprint, render_template, request,redirect ) 
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 #
-@bp.route('/', methods=['POST'])
+@bp.route('/', methods=['GET','POST'])
 def index():
-    print(request.form)
+    if request.method == 'POST':
+       print(request.form)
+       return redirect('/facts')
+    
     return 'Thanks for submitting a fun fact!'
 
 #nwe page
 @bp.route('/new')
 def new(): 
     return render_template('facts/new.html')
+
+
